95f745bfd986dd2b9bb879141ac94aff7e4d1775
Dorian <dorian.carle@isen.yncrea.fr>
2020-06-03 19:55:11 +0200

    il le faut

---
+++ src/main/java/fr/yncrea/cir3/jeuEchec/classChessGame/ChessGame.java
@@ -0,0 +1,18 @@
+package fr.yncrea.cir3.jeuEchec.classChessGame;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.yncrea.cir3.jeuEchec.domain.Pawn;
+public class ChessGame {
+	
+	private List<Pawn> pawns;
+	
+	public List<Pawn> chessGameInit(){
+		pawns = new ArrayList<>();
+		
+		
+		return pawns;
+	}
+}
